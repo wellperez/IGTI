91474fe54a78fb8296a1a5998b365ab7e7281a87
Wellington Perez <wellingtonaperez@gmail.com>
2020-11-30 15:20:31 -0300

    refactor: move functions in script file

--- desafio-modulo-1/js/script.js
+++ desafio-modulo-1/js/script.js
@@ -1,39 +1,106 @@
+function createParagraph(firstName, lastName, age) {
+  const para = document.createElement('P'); // Create a <p> node
+  para.textContent = `${firstName} ${lastName}, ${age} anos.`;
+  return para;
+}
+
+function createImg(src) {
+  const img = document.createElement('img');
+  img.src = src;
+  img.classList.add('circle');
+  return img;
+}
+
+function disableBtn() {
+  document.getElementById('user').addEventListener('keyup', () => {
+    const userInput = document.getElementById('user').value;
+    if (userInput !== '') {
+      document.getElementById('send').removeAttribute('disabled');
+    } else {
+      document.getElementById('send').setAttribute('disabled', null);
+    }
+  });
+}
+
 async function loadUsers() {
-  const response = await fetch("http://localhost:3000/results");
+  const response = await fetch('http://localhost:3000/results');
   const json = await response.json();
   const user = await Object.values(json);
   return user;
+}
+
+function createLi() {
+  const li = document.createElement('li');
+  li.classList.add('collection-item');
+  li.classList.add('avatar');
+  return li;
+}
+
+function createStatisticsParagraph(
+  maleGenderParam,
+  femaleGenderParam,
+  othersGendersParam,
+  sumOfAgesParam,
+  qntUserStatisticsParam,
+) {
+  const maleGender = document.createElement('P');
+  maleGender.textContent = `Sexo masculino: ${maleGenderParam}`;
+
+  const femaleGender = document.createElement('P');
+  femaleGender.textContent = `Sexo feminino: ${femaleGenderParam}`;
+
+  const othersGenders = document.createElement('P');
+  othersGenders.textContent = `Outros Gêneros: ${othersGendersParam}`;
+
+  const sumOfAges = document.createElement('P');
+  sumOfAges.textContent = `Soma das Idades: ${sumOfAgesParam}`;
+
+  const averageAge = document.createElement('P');
+  averageAge.textContent = `Média das Idades: ${Math.round(
+    sumOfAgesParam / qntUserStatisticsParam,
+    -2,
+  )}`;
+
+  const statistics = document.getElementById('statistics');
+
+  statistics.append(
+    maleGender,
+    femaleGender,
+    othersGenders,
+    sumOfAges,
+    averageAge,
+  );
 }
 
 async function handleSubmit(event) {
   // Evitando o refresh
   event.preventDefault();
 
-  var userInput = document.querySelector("#user");
-  var userInputValue = userInput.value.toLowerCase();
+  const userInput = document.querySelector('#user');
+  const userInputValue = userInput.value.toLowerCase();
 
   const users = await loadUsers();
 
-  const results = document.getElementById("results-ul");
-  results.textContent = "";
+  const results = document.getElementById('results-ul');
+  results.textContent = '';
 
-  const statistics = document.getElementById("statistics");
-  statistics.textContent = "";
+  const statistics = document.getElementById('statistics');
+  statistics.textContent = '';
 
   let ageStatistics = 0;
-  let genderStatistics = { male: 0, female: 0, others: 0 };
+  const genderStatistics = { male: 0, female: 0, others: 0 };
   let qntUserStatistics = 0;
 
-  users.map((user, index) => {
+  users.map((user) => {
     const firstName = user.name.first;
     const lastName = user.name.last;
-    const age = user.dob.age;
+    const { age } = user.dob;
     const picture = user.picture.large;
-    const gender = user.gender;
+    const { gender } = user;
 
     if (
-      firstName.toLowerCase().includes(userInputValue) ||
-      lastName.toLowerCase().includes(userInputValue)
+      firstName.toLowerCase().includes(userInputValue)
+      || lastName.toLowerCase().includes(userInputValue)
     ) {
       // ESTRUTURA PARA ADICIONAR OS USUÁRIOS NO HTML
       const li = createLi();
@@ -45,109 +112,45 @@ async function handleSubmit(event) {
 
       // ESTRUTURA PARA REALIZAR AS ESTATÍSTICAS
       ageStatistics += age;
-      qntUserStatistics++;
+      qntUserStatistics += 1;
 
-      if (gender === "female") {
-        genderStatistics.female++;
-      } else if (gender === "male") {
-        genderStatistics.male++;
+      if (gender === 'female') {
+        genderStatistics.female += 1;
+      } else if (gender === 'male') {
+        genderStatistics.male += 1;
       } else {
-        genderStatistics.others++;
+        genderStatistics.others += 1;
       }
     }
-    console.log(qntUserStatistics);
+
+    return true;
   });
 
   if (qntUserStatistics > 0) {
-    const h3 = document.getElementById("users-title");
+    const h3 = document.getElementById('users-title');
     h3.textContent = `${qntUserStatistics} usuário(s) encontrado(s)`;
-    document.getElementById("results-ul").removeAttribute("hidden");
+    document.getElementById('results-ul').removeAttribute('hidden');
 
     createStatisticsParagraph(
       genderStatistics.male,
       genderStatistics.female,
       genderStatistics.others,
       ageStatistics,
-      qntUserStatistics
+      qntUserStatistics,
     );
   } else {
-    alert("Nenhum usuário encontrado!");
+    alert('Nenhum usuário encontrado!');
     location.reload();
   }
 }
 
-function createLi() {
-  const li = document.createElement("li");
-  li.classList.add("collection-item");
-  li.classList.add("avatar");
-  return li;
-}
-
-function createStatisticsParagraph(
-  maleGenderParam,
-  femaleGenderParam,
-  othersGendersParam,
-  sumOfAgesParam,
-  qntUserStatisticsParam
-) {
-  const maleGender = document.createElement("P");
-  maleGender.textContent = `Sexo masculino: ${maleGenderParam}`;
-
-  const femaleGender = document.createElement("P");
-  femaleGender.textContent = `Sexo masculino: ${femaleGenderParam}`;
-
-  const othersGenders = document.createElement("P");
-  othersGenders.textContent = `Outros Gêneros: ${othersGendersParam}`;
-
-  const sumOfAges = document.createElement("P");
-  sumOfAges.textContent = `Soma das Idades: ${sumOfAgesParam}`;
-
-  const averageAge = document.createElement("P");
-  averageAge.textContent = `Média das Idades: ${Math.round(
-    sumOfAgesParam / qntUserStatisticsParam,
-    -2
-  )}`;
-
-  statistics.append(
-    maleGender,
-    femaleGender,
-    othersGenders,
-    sumOfAges,
-    averageAge
-  );
-}
-
-function createParagraph(firstName, lastName, age) {
-  const para = document.createElement("P"); // Create a <p> node
-  para.textContent = `${firstName} ${lastName}, ${age} anos.`;
-  return para;
-}
-
-function createImg(src) {
-  const img = document.createElement("img");
-  img.src = src;
-  img.classList.add("circle");
-  return img;
-}
-
-function disableBtn() {
-  document.getElementById("user").addEventListener("keyup", function () {
-    var userInput = document.getElementById("user").value;
-    if (userInput != "") {
-      document.getElementById("send").removeAttribute("disabled");
-    } else {
-      document.getElementById("send").setAttribute("disabled", null);
-    }
-  });
-}
-
 function start() {
-  var form = document.querySelector("form");
-  form.addEventListener("submit", handleSubmit);
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
 
   disableBtn();
 
-  document.getElementById("user").focus();
+  document.getElementById('user').focus();
 }
 
 start();
