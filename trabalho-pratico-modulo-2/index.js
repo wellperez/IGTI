289b48c97cd9e4f645155e3fdc5c0cc059f2b173
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-06 21:56:41 -0300

    feat: create itens 3 and 4 of schoolwork

--- trabalho-pratico-modulo-2/index.js
+++ trabalho-pratico-modulo-2/index.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
 import { promises as fs } from 'fs';
 
 async function readJsonFiles(file, state) {
-  console.log(state);
   if (state) {
     return JSON.parse(await fs.readFile(`states/${file}.json`));
   }
@@ -18,16 +18,58 @@ async function createStateFiles(states, cities) {
 
 async function lengthOfState(uf) {
   const cities = await readJsonFiles(uf, true);
-  console.log(cities.length);
   return cities.length;
 }
 
-const init = async () => {
+async function largestCities(states) {
+  const citiesInState = [];
+  for (const state of states) {
+    // eslint-disable-next-line no-await-in-loop
+    const length = await lengthOfState(state.Sigla);
+    const stateObject = {
+      state: state.Sigla,
+      cities: length,
+    };
+    citiesInState.push(stateObject);
+    console.log((`${state.Sigla} - ${length}`));
+  }
+
+  citiesInState.sort((a, b) => {
+    if (a.cities < b.cities) return 1;
+    if (a.cities > b.cities) return -1;
+    return 0;
+  });
+  return citiesInState.filter((_, index) => index < 5);
+}
+
+async function smallestCities(states) {
+  const citiesInState = [];
+  for (const state of states) {
+    // eslint-disable-next-line no-await-in-loop
+    const length = await lengthOfState(state.Sigla);
+    const stateObject = {
+      state: state.Sigla,
+      cities: length,
+    };
+    citiesInState.push(stateObject);
+    console.log((`${state.Sigla} - ${length}`));
+  }
+
+  citiesInState.sort((a, b) => {
+    if (a.cities > b.cities) return 1;
+    if (a.cities < b.cities) return -1;
+    return 0;
+  });
+  return citiesInState.filter((_, index) => index < 5).reverse();
+}
+
+async function init() {
   const states = await readJsonFiles('Estados.json');
   const cities = await readJsonFiles('Cidades.json');
 
-  createStateFiles(states, cities);
-  lengthOfState('SP');
-};
+  await createStateFiles(states, cities);
+  console.log(await largestCities(states));
+  console.log(await smallestCities(states));
+}
 
 init();
