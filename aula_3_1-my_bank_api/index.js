e1131ef94d5061251f4f2189bab9d6aee5dc7174
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-08 20:43:02 -0300

    chore: create initial version

---
+++ aula_3_1-my_bank_api/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import express from 'express';
+import { promises as fs } from 'fs';
+import accountsRouter from './routes/account.js';
+
+const { readFile, writeFile } = fs;
+
+const app = express();
+app.use(express.json());
+
+app.use('/account', accountsRouter);
+
+app.listen(3000, async () => {
+  try {
+    await readFile('accounts.json');
+    console.log('API Started!');
+  } catch {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+    writeFile('accounts.json', JSON.stringify(initialJson)).then(() => {
+      console.log('API Started and Created File!');
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+});
