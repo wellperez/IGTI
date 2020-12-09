d1038f34aa996ddec87ba80741cd17468d867379
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-09 15:37:43 -0300

    feat: create delete method

--- aula_3_1-my_bank_api/routes/accounts.js
+++ aula_3_1-my_bank_api/routes/accounts.js
@@ -63,4 +63,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    const { id } = req.params;
+    data.accounts = data.accounts.filter((acc) => acc.id !== Number(id));
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.end();
+  } catch (error) {
+    console.error(error);
+    res.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
 export default router;
