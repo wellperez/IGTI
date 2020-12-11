bd4fbd06a5f165cf4373868663f13b281bceeb44
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-11 13:51:24 -0300

    feat: add PUT method

--- desafio-modulo-2/routes/grades.js
+++ desafio-modulo-2/routes/grades.js
@@ -31,7 +31,39 @@ router.post('/', async (req, res, next) => {
 
     res.send(newGrade);
 
-    global.logger.info(`POST /account - ${JSON.stringify(newGrade)}`);
+    global.logger.info(`POST /grades - ${JSON.stringify(newGrade)}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/', async (req, res, next) => {
+  try {
+    const {
+      id, student, subject, type, value,
+    } = req.body;
+
+    if (!student || !subject || !type || value === null) {
+      throw new Error('Aluno, Matéria, Tipo e Nota são obrigatórios');
+    }
+
+    const data = JSON.parse(await readFile(global.filename));
+
+    const index = data.grades.findIndex((grade) => grade.id === Number(id));
+
+    if (index === -1) {
+      throw new Error('Registro Nao encontrado!');
+    }
+
+    data.grades[index].student = student;
+    data.grades[index].subject = subject;
+    data.grades[index].type = type;
+    data.grades[index].value = value;
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.send(data.grades[index]);
+    global.logger.info(`PUT /grades - ${JSON.stringify(data.grades[index])}`);
   } catch (error) {
     next(error);
   }
